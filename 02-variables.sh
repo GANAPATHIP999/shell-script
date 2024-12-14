4ebd0362aae53d092c78c94474cec7a52d8eefc4
GANAPTHIP999 <ganapathipemmani999@gmail.com>
2024-12-14 08:35:20 -0500

    some-message

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-this is again sample script
+#!/bin/bash
+
+echo "Ramesh:: Hello suresh,How are you?"
+ech "Suresh:: Hi Ramesh,I am fine"
+echo "Ramesh: I am fine too. How is your work"
+echo "suresh:: not bad. I am thinking about to upgrde my skill to DevOps"
