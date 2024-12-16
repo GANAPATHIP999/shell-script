1dbaaff5c50679db6bb7c4a1383a5b52dcea0b9c
GANAPTHIP999 <ganapathipemmani999@gmail.com>
2024-12-16 11:50:53 -0500

    function

--- 14-install-packges.sh
+++ 14-install-packges.sh
@@ -5,6 +5,11 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -35,10 +40,10 @@ do
   dnf list installed $i &>>$LOGFILE
   if [ $? -ne 0 ]
   then 
-       echo " $i alredy installed...SKIPPING"
+       echo -e " $i alredy installed...$Y SKIPPING $N"
    else 
       echo " $i not installed...Need to install"
-      
+
   fi
 
 
