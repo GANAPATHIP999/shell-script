2bd4226040166a0eea5e570239daa97cdc0c303a
GANAPTHIP999 <ganapathipemmani999@gmail.com>
2024-12-16 10:33:39 -0500

    function

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,8 +3,8 @@
 USERID=$(id -u)
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
