6942c443024a0306c747e5a950699cdc4b215c1a
GANAPTHIP999 <ganapathipemmani999@gmail.com>
2024-12-18 08:12:37 -0500

    function

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -6,7 +6,7 @@ COURSE="Devops from current Script"
  echo "Process ID of current shell script:$$"
 
  #./16-other-script.sh 
- source./16-other-script.sh 
+ source ./16-other-script.sh 
 
  echo "After calling other script, course: $COURSE"
  echo "Process ID of current shell script: $$"
