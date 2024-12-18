aabb2f3157ff66f913444814f69bc07fc15ecb5d
GANAPTHIP999 <ganapathipemmani999@gmail.com>
2024-12-18 10:37:15 -0500

    function

---
+++ 17-set.sh
@@ -0,0 +1,28 @@
+#!/bin /bash
+
+set -e
+
+failure(){
+    echo "Failed at $lineno: $msg"
+
+}
+
+trap 'failure ${LINENO} "BASH_COMMAND"'ERR
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+  then 
+    echo "please run this script with root access."
+    exit 1
+else 
+
+    echo "you are super user."
+fi
+
+dnf install mysql -y
+
+dnf install git -y
+
+
+echo "is script proceeding?"
