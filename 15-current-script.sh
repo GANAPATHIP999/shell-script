9721004546f8669f8919fbfb550921dd09ef9355
GANAPTHIP999 <ganapathipemmani999@gmail.com>
2024-12-18 07:59:51 -0500

    function

---
+++ 15-current-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COURSE="Devops from current Script"
+
+ echo "Before calling other script, course: $COURSE"
+ echo "Process ID of current shell script:$$"
+
+ #./16-other-script.share
+
+ echo "After calling other script, course: $COURSE"
+ echo "Process ID of current shell script: $$"
