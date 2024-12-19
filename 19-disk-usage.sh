f187fb3c7814ff599a5cdf240e70542685523f82
GANAPTHIP999 <ganapathipemmani999@gmail.com>
2024-12-19 09:23:08 -0500

    function

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -4,9 +4,10 @@ DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
 
 while 
-do 
+ do 
   USAGE=$(echo $line |  awk -F " " ' {print $6F}' | cut -d "%" -f1)
   FOLDER=$(echo $line |  awk -F " " ' {print $NF}')
+  
   if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         echo "$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE"
